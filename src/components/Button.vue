87c8be15c230685234b441b230a049dc8ba80e1a
Syaifudin Alkatiri <sfnalkatiri@gmail.com>
2021-10-30 12:08:23 +0700

    delete, toggle, add task

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+	<button @click="onClick()" :style="{ background: color }" clas="btn">
+		{{ text }}
+	</button>
+</template>
+<script>
+export default {
+	name: "Button",
+	props: {
+		text: String,
+		color: String,
+	},
+};
+</script>
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<Button text="Add task" color="green" />
+	</header>
+</template>
+<script>
+import Button from "./Button.vue";
+export default {
+	name: "Header",
+	components: {
+		Button,
+	},
+	props: {
+		title: String,
+	},
+};
+</script>
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
